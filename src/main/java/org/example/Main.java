cfebcc5a89983cf39d09dcbed334bb5422747aec
vishwsingh <khayekmusic@gmail.com>
2023-03-22 10:51:11 -0400

    Adding

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Love");
+        }
+    }
